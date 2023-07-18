8755a6388d7e23d0580128dc292fe8625ee1a85b
KareddyPavithra <kareddy.p@northeastern.edu>
2023-07-18 15:40:46 -0700

    Setting up the models

---
+++ workspace/restaurant/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .  import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    #path("", views.sayHello, name="say hello"),
+    path("",views.index,name='index'),
+
+
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+]
